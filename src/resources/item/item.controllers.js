72bc76220394ca7dff3b947f5ecad399e240065b
hir0o <mk.9.q.kyu@gmail.com>
2021-08-14 00:28:34 +0900

    finish

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,18 @@
-import { crudControllers } from '../../utils/crud'
+// import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
 
-export default crudControllers(Item)
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+  console.log(await Item.find({}).exec())
+}
+
+run()
+
+// export default crudControllers(Item)
