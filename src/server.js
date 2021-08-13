34f2abf20f64de3175923e2d48a7a1e8761d979b
hir0o <mk.9.q.kyu@gmail.com>
2021-08-13 18:51:39 +0900

    lesson1 終わり

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, { Router } from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,27 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/get', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.use('/api', router)
+
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.get('/data:id', [log, log, log], (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
